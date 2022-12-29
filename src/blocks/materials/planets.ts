9090d7c2e3a83b0ce98e96426b2921710b50b7a9
Simon Guest <simon@code.org>
2022-12-29 18:39:43 +0000

    Added solar system example

--- src/blocks/materials/planets.ts
+++ src/blocks/materials/planets.ts
@@ -6,8 +6,14 @@ export let planets = {
         let input = this.appendDummyInput()
             .appendField('planet: ');
         let options = [
+            [{'src': '../assets/materials/planets/mercury.jpg', 'width': 25, 'height': 25, 'alt': 'Mercury'}, 'planets/mercury.jpg'],
+            [{'src': '../assets/materials/planets/venus.jpg', 'width': 25, 'height': 25, 'alt': 'Venus'}, 'planets/venus.jpg'],
             [{'src': '../assets/materials/planets/earth.jpg', 'width': 25, 'height': 25, 'alt': 'Earth'}, 'planets/earth.jpg'],
+            [{'src': '../assets/materials/planets/mars.jpg', 'width': 25, 'height': 25, 'alt': 'Mars'}, 'planets/mars.jpg'],
             [{'src': '../assets/materials/planets/jupiter.jpg', 'width': 25, 'height': 25, 'alt': 'Jupiter'}, 'planets/jupiter.jpg'],
+            [{'src': '../assets/materials/planets/saturn.jpg', 'width': 25, 'height': 25, 'alt': 'Saturn'}, 'planets/saturn.jpg'],
+            [{'src': '../assets/materials/planets/uranus.jpg', 'width': 25, 'height': 25, 'alt': 'Uranus'}, 'planets/uranus.jpg'],
+            [{'src': '../assets/materials/planets/neptune.jpg', 'width': 25, 'height': 25, 'alt': 'Neptune'}, 'planets/neptune.jpg'],
         ];
         input.appendField(new Blockly.FieldDropdown(options), 'MATERIAL');
         this.setOutput(true, "MATERIAL");
