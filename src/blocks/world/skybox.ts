17a290efdc8d0ad66f4397f619c4008625eca1f6
Simon Guest <simon@code.org>
2022-12-31 10:25:22 -0800

    Updated path for skybox preview image

--- src/blocks/world/skybox.ts
+++ src/blocks/world/skybox.ts
@@ -6,11 +6,11 @@ export let skybox = {
         let input = this.appendDummyInput()
             .appendField('set sky to: ');
         let options = [
-            [{'src': '../assets/env/puresky_PREVIEW.jpg', 'width': 25, 'height': 25, 'alt': 'puresky'}, 'puresky'],
-            [{'src': '../assets/env/clouds_PREVIEW.jpg', 'width': 25, 'height': 25, 'alt': 'clouds'}, 'clouds'],
-            [{'src': '../assets/env/deep_space_PREVIEW.jpg', 'width': 25, 'height': 25, 'alt': 'deep_space'}, 'deep_space'],
-            [{'src': '../assets/env/orlando_stadium_PREVIEW.jpg', 'width': 25, 'height': 25, 'alt': 'orlando_stadium'}, 'orlando_stadium'],
-            [{'src': '../assets/env/modern_buildings_PREVIEW.jpg', 'width': 25, 'height': 25, 'alt': 'modern_buildings'}, 'modern_buildings'],
+            [{'src': './assets/env/puresky_PREVIEW.jpg', 'width': 25, 'height': 25, 'alt': 'puresky'}, 'puresky'],
+            [{'src': './assets/env/clouds_PREVIEW.jpg', 'width': 25, 'height': 25, 'alt': 'clouds'}, 'clouds'],
+            [{'src': './assets/env/deep_space_PREVIEW.jpg', 'width': 25, 'height': 25, 'alt': 'deep_space'}, 'deep_space'],
+            [{'src': './assets/env/orlando_stadium_PREVIEW.jpg', 'width': 25, 'height': 25, 'alt': 'orlando_stadium'}, 'orlando_stadium'],
+            [{'src': './assets/env/modern_buildings_PREVIEW.jpg', 'width': 25, 'height': 25, 'alt': 'modern_buildings'}, 'modern_buildings'],
             
         ];
         input.appendField(new Blockly.FieldDropdown(options), 'SKYBOX');
